8a608fb28ba93d109e0b8f80bd498736262b86d0
Gonzalo Cabezas Núñez <170438344+sandokanCat@users.noreply.github.com>
2025-04-19 03:22:50 +0200

    Update estopa.js

--- js/estopa.js
+++ js/estopa.js
@@ -1,3 +1,9 @@
+console.log(
+    "%c💻 ¡Gracias por curiosear el código! ¿Te apetece un café virtual mientras terminamos el proyecto? ☕\n" +
+    "🚧 PD: Estamos en construcción activa. ¡Feedback bienvenido!",
+    "color: #2ecc71; font-weight: bold; font-size: 16px;"
+);
+
 import * as cookies from './cookies.js';
 cookies.initCookieBar();
 
